b03adb0ef7816ec2c9e954118cd3fdd136a12066
Lday <vbuihjgnre@gmail.com>
2023-02-14 14:59:15 +0300

    refactoring class component 'Search' to functional component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,95 +1,89 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
-class Search extends Component {
-    state = {
-        search: '',
-        typeSearch: '',
-    };
+function Search({ searchMovies }) {
+    const [search, setSearch] = useState('matrix');
+    const [typeSearch, setTypeSearch] = useState('');
 
-    handleKey = (event) => {
+    const handleKey = (event) => {
         if (event.key === 'Enter') {
-            this.props.searchMovies(this.state.search, this.state.typeSearch);
+            searchMovies(search, typeSearch);
         }
     };
 
-    handleType = (event) => {
-        this.setState(
-            () => ({ [event.target.name]: event.target.value }),
-            () => {
-                this.props.searchMovies(this.state.search, this.state.typeSearch);
-            }
-        );
+    const handleType = (event) => {
+        setTypeSearch(event.target.value);
     };
 
-    render() {
-        const { typeSearch } = this.state;
-        return (
-            <div className='row'>
-                <div className='input-field '>
-                    <input
-                        placeholder='search'
-                        type='search'
-                        className='validate'
-                        value={this.state.search}
-                        onChange={(e) => this.setState({ search: e.target.value })}
-                        onKeyDown={this.handleKey}
-                    />
-                    <button
-                        className='waves-effect waves-light cyan btn search-btn'
-                        onClick={() => this.props.searchMovies(this.state.search, this.state.typeSearch)}
-                    >
-                        Search
-                    </button>
-                    <div className='search-type'>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='typeSearch'
-                                type='radio'
-                                value=''
-                                onChange={this.handleType}
-                                checked={typeSearch === ''}
-                            />
-                            <span>All</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='typeSearch'
-                                type='radio'
-                                value='movie'
-                                onChange={this.handleType}
-                                checked={typeSearch === 'movie'}
-                            />
-                            <span>Movie</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='typeSearch'
-                                type='radio'
-                                value='series'
-                                onChange={this.handleType}
-                                checked={typeSearch === 'series'}
-                            />
-                            <span>Series</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='typeSearch'
-                                type='radio'
-                                value='episode'
-                                onChange={this.handleType}
-                                checked={typeSearch === 'episode'}
-                            />
-                            <span>Episode</span>
-                        </label>
-                    </div>
+    useEffect(() => {
+        searchMovies(search, typeSearch);
+    }, [typeSearch]);
+
+    return (
+        <div className='row'>
+            <div className='input-field '>
+                <input
+                    placeholder='search'
+                    type='search'
+                    className='validate'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKey}
+                />
+                <button
+                    className='waves-effect waves-light cyan btn search-btn'
+                    onClick={() => searchMovies(search, typeSearch)}
+                >
+                    Search
+                </button>
+                <div className='search-type'>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='typeSearch'
+                            type='radio'
+                            value=''
+                            onChange={handleType}
+                            checked={typeSearch === ''}
+                        />
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='typeSearch'
+                            type='radio'
+                            value='movie'
+                            onChange={handleType}
+                            checked={typeSearch === 'movie'}
+                        />
+                        <span>Movie</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='typeSearch'
+                            type='radio'
+                            value='series'
+                            onChange={handleType}
+                            checked={typeSearch === 'series'}
+                        />
+                        <span>Series</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='typeSearch'
+                            type='radio'
+                            value='episode'
+                            onChange={handleType}
+                            checked={typeSearch === 'episode'}
+                        />
+                        <span>Episode</span>
+                    </label>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export { Search };
